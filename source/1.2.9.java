2b2b4a867df958cea6c067725988573d4fde928f
LinfengSun <loveconan900504@gmail.com>
2018-02-05 21:04:16 -0500

    Create 1.2.9.java

---
+++ source/1.2.9.java
@@ -0,0 +1,35 @@
+/*
+Instrument BinarySearch (page 47) to use a Counter to count the total number
+of keys examined during all searches and then print the total after all searches are complete.
+Hint : Create a Counter in main() and pass it as an argument to rank().
+*/
+
+import java.util.Arrays;
+public class BinarySearch
+{
+  public static int rank(int key, int[] a)
+  { // Array must be sorted.
+    int lo = 0;
+    int hi = a.length - 1;
+    while (lo <= hi)
+    { // Key is in a[lo..hi] or not present.
+      int mid = lo + (hi - lo) / 2;
+      if (key < a[mid]) hi = mid - 1;
+      else if (key > a[mid]) lo = mid + 1;
+      else return mid;
+    }
+    return -1;
+  }
+  
+  public static void main(String[] args)
+  {
+    int[] whitelist = In.readInts(args[0]);
+    Arrays.sort(whitelist);
+    while (!StdIn.isEmpty())
+    { // Read key, print if not in whitelist.
+    int key = StdIn.readInt();
+    if (rank(key, whitelist) < 0)
+    StdOut.println(key);
+  }
+ }
+}
