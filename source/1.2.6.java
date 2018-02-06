f51cb51af9cab3399e873b40ef3400987cee00a7
LinfengSun <loveconan900504@gmail.com>
2018-02-05 20:37:28 -0500

    Update 1.2.6.java

--- source/1.2.6.java
+++ source/1.2.6.java
@@ -1,3 +1,12 @@
+/*
+A string s is a circular rotation of a string t if it matches when the characters
+are circularly shifted by any number of positions; e.g., ACTGACG is a circular shift of
+TGACGAC, and vice versa. Detecting this condition is important in the study of genomic
+sequences. Write a program that checks whether two given strings s and t are circular
+shifts of one another. Hint : The solution is a one-liner with indexOf(), length(), and
+string concatenation.
+*/
+
 class exercise {
     private static boolean isCircular(String a, String b) {
 	    return a.length() == b.length() && (a+a).contains(b);
