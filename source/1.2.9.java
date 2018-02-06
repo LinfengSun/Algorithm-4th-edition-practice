27a042cae879155b8f6b75fea17c39ff04cc89bc
LinfengSun <loveconan900504@gmail.com>
2018-02-05 21:31:28 -0500

    Update 1.2.9.java

--- source/1.2.9.java
+++ source/1.2.9.java
@@ -7,15 +7,15 @@ Hint : Create a Counter in main() and pass it as an argument to rank().
 import java.util.Arrays;
 public class BinarySearch
 {
-  public static int rank(int key, int[] a)
+  public static int rank(int key, int[] a, Counter counter)
   { // Array must be sorted.
     int lo = 0;
     int hi = a.length - 1;
     while (lo <= hi)
     { // Key is in a[lo..hi] or not present.
       int mid = lo + (hi - lo) / 2;
-      if (key < a[mid]) hi = mid - 1;
-      else if (key > a[mid]) lo = mid + 1;
+      if (key < a[mid]) hi = mid - 1; counter.increment();
+      else if (key > a[mid]) lo = mid + 1; counter.increment();
       else return mid;
     }
     return -1;
@@ -24,12 +24,14 @@ public class BinarySearch
   public static void main(String[] args)
   {
     int[] whitelist = In.readInts(args[0]);
+    Counter counter = new Counter(keys);
     Arrays.sort(whitelist);
     while (!StdIn.isEmpty())
     { // Read key, print if not in whitelist.
-    int key = StdIn.readInt();
-    if (rank(key, whitelist) < 0)
-    StdOut.println(key);
+      int key = StdIn.readInt();
+      if (rank(key, whitelist, counter) < 0)
+      StdOut.println(key);
+      StdOut.println(counter);
     }
   }
 }
